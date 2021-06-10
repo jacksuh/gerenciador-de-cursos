01a17ee969b24d34d2cb0a954fae2c81012537bb
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-06-10 06:29:44 -0300

    Collection

---
+++ src/br/com/jackson/QualColecaoUsar.java
@@ -0,0 +1,10 @@
+package br.com.jackson;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class QualColecaoUsar {
+    public static void main(String[] args) {
+        Collection<Aluno> alunos = new ArrayList<>();
+    }
+}
