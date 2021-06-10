d15fe04beb6d6c59fa359910a2a45377ffbfce32
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-06-10 06:53:29 -0300

    Collection map

--- src/br/com/jackson/Curso.java
+++ src/br/com/jackson/Curso.java
@@ -5,56 +5,60 @@ import br.com.jackson.Aluno;
 import java.util.*;
 
 public class Curso {
-
-	private String nome;
-	private String instrutor;
-	private List<Aula> aulas = new LinkedList<Aula>();
+		private String nome;
+		private String instrutor;
+		private List<Aula> aulas = new LinkedList<Aula>();
+		private Set<Aluno> alunos = new HashSet<>();
+		private Map<Integer, Aluno> matriculaParaAluno = new HashMap<>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
 		this.instrutor = instrutor;
 	}
 
-
-
-	public String getNome() {
+		public String getNome() {
 		return nome;
 	}
 
-	public String getInstrutor() {
+		public String getInstrutor() {
 		return instrutor;
 	}
 
-	//para não mudar a lista
-	public List<Aula> getAulas() {
+		public List<Aula> getAulas() {
 		return Collections.unmodifiableList(aulas);
 	}
 
-	public void adiciona(Aula aula) {
-		this.aulas.add(aula);
-	}
-
-	private Set<Aluno> alunos = new HashSet<>();
-
-	public void matricula(Aluno aluno){
-		this.alunos.add(aluno);
-	}
-
-	public Set<Aluno> getAlunos() {
+		public Set<Aluno> getAlunos() {
 		return Collections.unmodifiableSet(alunos);
 	}
 
-	public boolean estaMatriculado(Aluno aluno) {
+		public void adiciona(Aula aula) {
+		this.aulas.add(aula);
+	}
+
+		public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+
+		@Override
+		public String toString() {
+		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal()
+				+ ", aulas: + " + this.aulas + "]";
+	}
+
+		public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+		this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
+	}
+
+		public boolean estaMatriculado(Aluno aluno) {
 		return this.alunos.contains(aluno);
 	}
 
-
-	@Override
-	public int hashCode(){
-		return this.nome.hashCode();
+		public Aluno buscaMatriculado(int numero) {
+		if(!matriculaParaAluno.containsKey(numero))
+			throw new NoSuchElementException();
+		return matriculaParaAluno.get(numero);
 	}
 
-
-
-
-}
+	}
